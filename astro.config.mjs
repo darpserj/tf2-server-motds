cbc02bf46ff6237970c85633d313be34676a4f6f
darpserj <sergiupopa999@outlook.com>
2025-02-23 12:03:23 +0200

    - added rollup-plugin-vizualizer (not really too representative but still) - added css-nano

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// import { visualizer } from "rollup-plugin-visualizer";
+
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+    //   visualizer({
+    //     emitFile: false,
+    //     filename: "stats.html",
+    // })
+]
   },
   site: 'https://darpserj.github.io',
   base: 'tf2-server-motds',
